4a0f30313887875c4031ad3c60e5bd22c28d8282
document10 <popescul2222georgeteodor@gmail.com>
2023-08-31 20:10:43 +0300

    Update build-x86-images.sh

--- build-x86-images.sh
+++ build-x86-images.sh
@@ -32,7 +32,7 @@ xbps-install -yu xbps
 xbps-install -Syu
 xbps-install -y qemu-user-static liblz4
 IMG=void-live-${ARCH}-${DATE}-openbox.iso
-PKGS="pipewire alsa-pipewire dialog cryptsetup lvm2 mdadm void-docs-browse xtools-minimal xmirror grub-i386-efi grub-x86_64-efi xorg xorg-input-drivers xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf openbox obconf lxappearance lxrandr lightdm octoxbps xbps alacritty neofetch lightdm-gtk-greeter"
+PKGS="pipewire alsa-pipewire dialog cryptsetup lvm2 mdadm void-docs-browse xtools-minimal xmirror grub-i386-efi grub-x86_64-efi xterm xclock menumaker micro exa neofetch xorg xorg-input-drivers xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf openbox obconf lxappearance lxrandr lightdm octoxbps xbps alacritty neofetch lightdm-gtk-greeter"
 SERVICES="sshd acpid dhcpcd wpa_supplicant lightdm dbus polkitd"
 LIGHTDM_SESSION='openbox'
 mkdir -p "$INCLUDEDIR"/etc/lightdm
